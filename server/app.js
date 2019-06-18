f1da16dd74ea31fcafe6b5a8d6d7f2ec7b77ba3c
GianFire <gian.delprado94@gmail.com>
2019-06-18 10:25:59 -0700

    removes express '/' route

--- server/app.js
+++ server/app.js
@@ -26,8 +26,4 @@ app.use(bodyParser.json());
 app.use('/api/users', require('./routes/users'));
 app.use('/api/blogs', require('./routes/blogs'));
 
-app.get('/', (req, res) => {
-  res.status(200).send('boop gimmie da loot');
-});
-
 module.exports = app;
